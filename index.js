f440ebad29348587a4f008cf443e2b0ce3229047
Allen <arusman1987@gmail.com>
2023-04-25 21:57:44 +0800

    dsfsdfdsf

--- index.js
+++ index.js
@@ -12,3 +12,76 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 
 
+function toonIn() {
+    window.location.href = "userpage.html";
+  }
+
+
+
+
+
+
+
+
+  const samplemodals = document.querySelectorAll('.samplemodal');
+  const modalImage = document.getElementById('modalImage');
+  
+  const images = [
+       'images/tomorrow.jpg',
+      'images/asia.jpg',
+      'images/holo love.jpg',
+      'images/the villains.jpg',
+      'images/blade.png',
+      'images/2521.jpg',
+
+  ];
+  
+  function handleSampleModalClick(index) {
+    modalImage.src = images[index % images.length];
+  }
+  
+  for (let i = 0; i < samplemodals.length; i++) {
+    samplemodals[i].addEventListener('click', function () {
+      handleSampleModalClick(i);
+    });
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   document.addEventListener('DOMContentLoaded', function () {
+//     const samplemodals = document.querySelectorAll('.samplemodal');
+//     const modalImage = document.getElementById('modalImage');
+//     const images = [
+//       'images/tomorrow.jpg',
+//       'images/asia.jpg',
+//       'images/holo love.jpg',
+//       'images/the villains.jpg',
+//       'images/blade.png',
+//       'images/2521.jpg',
+      
+//       // Add more images as needed
+//     ];
+  
+//     samplemodals.forEach((samplemodal, index) => {
+//       samplemodal.addEventListener('click', function () {
+//         modalImage.src = images[index % images.length];
+//       });
+//     });
+//   });
+  
