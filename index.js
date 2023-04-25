7224c7e2cbce2e03e300c364fcd18ea08c9bf534
Allen <arusman1987@gmail.com>
2023-04-26 00:38:43 +0800

    dfsdfdsf

--- index.js
+++ index.js
@@ -1,87 +1,60 @@
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
-// myModal.addEventListener('shown.bs.modal', () => {
-//   myInput.focus()
-// })
-
-
-
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-
 
 function toonIn() {
     window.location.href = "userpage.html";
   }
 
+          //  array shortcut for collapseimages using for loop
+const collapseImages = [];
+const totalImages = 12;
+const basePath = 'images/edited/imagesedits';
+
+for (let i = 1; i <= totalImages; i++) {
+  collapseImages.push(`${basePath}${i}.png`);
+}
+
+            // array shortcut for images using for loop
+
+            
+const images = [];
+const totalImages2 = 12;
+const basePath2 = 'images/edited/imagesedits';
+
+for (let i = 1; i <= totalImages2; i++) {
+images.push(`${basePath2}${i}.png`);
+}
+
+const samplemodals = document.querySelectorAll('.samplemodal');
+const modalImage = document.getElementById('modalImage');
+
+
+// Function to set the background image for the .samplemodal element
+function setSampleModalBackground(index) {
+  samplemodals[index].style.backgroundImage = `url(${images[index % images.length]})`;
+}
+
+function maramingPicture(index) {
+  modalImage.src = images[index % images.length];
+
+  // Update the image source inside the collapse element
+ 
+    const collapseImage = document.querySelector('#collapsepix');
+  collapseImage.src = collapseImages[index % collapseImages.length];
+
+}
+
+for (let i = 0; i < samplemodals.length; i++) {
+  // Set the background image for each .samplemodal element
+  setSampleModalBackground(i);
+
+  samplemodals[i].addEventListener('click', function () {
+    maramingPicture(i);
+  });
+}
 
 
 
 
 
-
-
-  const samplemodals = document.querySelectorAll('.samplemodal');
-  const modalImage = document.getElementById('modalImage');
-  
-  const images = [
-       'images/tomorrow.jpg',
-      'images/asia.jpg',
-      'images/holo love.jpg',
-      'images/the villains.jpg',
-      'images/blade.png',
-      'images/2521.jpg',
-
-  ];
-  
-  function handleSampleModalClick(index) {
-    modalImage.src = images[index % images.length];
-  }
-  
-  for (let i = 0; i < samplemodals.length; i++) {
-    samplemodals[i].addEventListener('click', function () {
-      handleSampleModalClick(i);
-    });
-  }
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   document.addEventListener('DOMContentLoaded', function () {
-//     const samplemodals = document.querySelectorAll('.samplemodal');
-//     const modalImage = document.getElementById('modalImage');
-//     const images = [
-//       'images/tomorrow.jpg',
-//       'images/asia.jpg',
-//       'images/holo love.jpg',
-//       'images/the villains.jpg',
-//       'images/blade.png',
-//       'images/2521.jpg',
-      
-//       // Add more images as needed
-//     ];
-  
-//     samplemodals.forEach((samplemodal, index) => {
-//       samplemodal.addEventListener('click', function () {
-//         modalImage.src = images[index % images.length];
-//       });
-//     });
-//   });
-  
